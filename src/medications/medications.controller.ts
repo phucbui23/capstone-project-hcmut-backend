d5afc89e75285d58ba1a26ca46c894255915b603
Hoàng Phúc <hoangphuc.osa@gmail.com>
2023-03-21 21:42:59 +0700

    feat: Medication apis

--- src/medications/medications.controller.ts
+++ src/medications/medications.controller.ts
@@ -5,10 +5,15 @@ import {
   Get,
   Param,
   Patch,
+  Post,
   Query,
+  Res,
 } from '@nestjs/common';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
-import { Medication, Prisma } from '@prisma/client';
+import { ApiBody, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+import { PAGINATION } from 'src/constant';
+import { CreateMedicationDto } from './dto/medications.dto';
 
 import { MedicationsService } from './medications.service';
 
@@ -18,27 +23,80 @@ export class MedicationsController {
   constructor(private readonly medicationsService: MedicationsService) {}
 
   @ApiQuery({
+    name: 'page',
+    required: false,
+    type: String,
+    description: 'Page of list',
+  })
+  @ApiQuery({
+    name: 'perPage',
+    required: false,
+    type: Number,
+    description: 'Number of record per page',
+  })
+  @ApiQuery({
+    name: 'field',
+    required: false,
+    enum: ['id', 'code', 'name', 'description', 'updatedAt', 'createdAt'],
+    type: String,
+    description: 'Sorting field',
+  })
+  @ApiQuery({
+    name: 'order',
+    required: false,
+    enum: ['desc', 'asc'],
+    type: String,
+    description: 'Sorting order',
+  })
+  @ApiQuery({
     name: 'keyword',
     required: false,
     type: String,
+    description: 'Query keyword',
   })
   @Get()
-  async findAll(@Query('keyword') keyword?: string): Promise<Medication[]> {
-    return await this.medicationsService.findAll({
-      where: {
-        name: {
-          contains: keyword,
-          mode: 'insensitive',
-        },
-      },
-    });
+  async getListMedications(
+    @Query('page') page: number = 1,
+    @Query('perPage') perPage: number = PAGINATION.PERPAGE,
+    @Query('field') field: string = 'updatedAt',
+    @Query('order') order: string = 'desc',
+    @Query('keyword') keyword: string = '',
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const result = await this.medicationsService.findAll(
+      page,
+      perPage,
+      field,
+      order,
+      keyword,
+    );
+
+    res.set('X-Total-Count', String(result.meta.total));
+    return result;
   }
 
+  @ApiBody({ type: CreateMedicationDto })
+  @Post('create')
+  async createOne(@Body() data: Prisma.MedicationCreateInput) {
+    return await this.medicationsService.createOne(data);
+  }
+
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'Medication id',
+  })
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Medication> {
+  async findOne(@Param('id') id: string) {
     return await this.medicationsService.findOne({ id: +id });
   }
 
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'Medication id',
+  })
+  @ApiBody({ type: CreateMedicationDto })
   @Patch(':id')
   async updateOne(
     @Param('id') id: string,
@@ -50,8 +108,13 @@ export class MedicationsController {
     });
   }
 
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'Medication id',
+  })
   @Delete(':id')
-  async deleteOne(@Param('id') id: string): Promise<Medication> {
+  async deleteOne(@Param('id') id: string) {
     return await this.medicationsService.deleteOne({ id: +id });
   }
 }
--- src/medications/medications.service.ts
+++ src/medications/medications.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { Medication, Prisma } from '@prisma/client';
+import { createPaginator } from 'prisma-pagination';
 
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -7,31 +8,76 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class MedicationsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findAll(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.MedicationWhereUniqueInput;
-    where?: Prisma.MedicationWhereInput;
-    orderBy?: Prisma.MedicationOrderByWithRelationInput;
-  }): Promise<Medication[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prismaService.medication.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
+  async findAll(
+    page: number,
+    perPage: number,
+    field: string,
+    order: string,
+    keyword: string,
+  ) {
+    const paginate = createPaginator({ perPage });
+    const result = await paginate(
+      this.prismaService.medication,
+      {
+        where: {
+          OR: [
+            {
+              code: {
+                contains: keyword,
+              },
+            },
+            {
+              name: {
+                contains: keyword,
+              },
+            },
+          ],
+        },
+        select: {
+          id: true,
+          code: true,
+          name: true,
+          description: true,
+        },
+        orderBy: {
+          [field]: order,
+        },
+      },
+      { page },
+    );
+    return result;
+  }
+
+  async createOne(data: Prisma.MedicationCreateInput) {
+    return this.prismaService.medication.create({
+      data,
     });
   }
 
   async findOne(where: Prisma.MedicationWhereUniqueInput): Promise<Medication> {
-    return this.prismaService.medication.findUnique({ where });
+    let result;
+    try {
+      result = await this.prismaService.medication.findUniqueOrThrow({ where });
+    } catch (error) {
+      throw new BadRequestException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'No medication found.',
+      });
+    }
+    return result;
   }
 
-  async deleteOne(
-    where: Prisma.MedicationWhereUniqueInput,
-  ): Promise<Medication> {
-    return this.prismaService.medication.delete({ where });
+  async deleteOne(where: Prisma.MedicationWhereUniqueInput) {
+    let result;
+    try {
+      result = await this.prismaService.medication.delete({ where });
+    } catch (error) {
+      throw new BadRequestException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'No Medication found.',
+      });
+    }
+    return result;
   }
 
   async updateOne({
@@ -40,10 +86,19 @@ export class MedicationsService {
   }: {
     where: Prisma.MedicationWhereUniqueInput;
     data: Prisma.MedicationUpdateInput;
-  }): Promise<Medication> {
-    return this.prismaService.medication.update({
-      where,
-      data,
-    });
+  }) {
+    let result;
+    try {
+      result = this.prismaService.medication.update({
+        where,
+        data,
+      });
+    } catch (error) {
+      throw new BadRequestException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'No Medication found.',
+      });
+    }
+    return result;
   }
 }

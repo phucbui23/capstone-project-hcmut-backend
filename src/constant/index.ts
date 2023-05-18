22c553211dd437d1a5bda4ba530c14a4a3351c16
Hoàng Phúc <hoangphuc.osa@gmail.com>
2023-05-19 04:29:04 +0700

    update: alert doctor if skip many times

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -14,3 +14,5 @@ export const MILLISECONDS_LAST_WEEK = 7 * 24 * 60 * 60 * 1000;
 
 export const PROFILE_IMAGE_MAX_SIZE = 1024000; // 1mb
 export const IMAGE_MAX_SIZE = 25000000; // 25mb
+
+export const SKIP_ALERT_RATE = 0.3;
